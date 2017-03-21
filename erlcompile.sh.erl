8b3fa5af6d9a35935ebcc971c924366b20a9f8c7
zsoci <zsolt.laky@erlang-solutions.com>
2017-03-21 18:45:07 +0100

    next version

--- erlcompile.sh.erl
+++ erlcompile.sh.erl
@@ -1,6 +1,6 @@
 #!/usr/bin/env escript
 %% -*- erlang -*-
-%%! -s inets start -sname interlc 
+%%! -s inets start -name interlc 
 %% add the following into idea external tools calling this script
 %% --i $Sourcepath$:$ProjectFileDir$/lib
 %% --o $FileParentDir$/ebin
@@ -22,21 +22,29 @@ do_compile([Source | Rest], Options) ->
   io:format("Node:~p", [Node]),
   Cookie = proplists:get_value(cookie, Options),
   erlang:set_cookie(node(), Cookie),
-  Module = list_to_atom(filename:basename(Source, ".erl")),
-  OutputDir = get_module_beam_dir(Node, Module),
-  io:format("OutputDir:~p", [OutputDir]),
-  CompileOptions = [{outdir, OutputDir},
-                    verbose, report_errors, report_warnings,
-                    debug_info, warn_unused_import, warn_obsolete_guard,
-                    warn_export_all |
-                    proplists:get_value(includes, Options) ],
-    case compile:file(Source, CompileOptions) of
-        {ok, _} ->
-            load_a_module(Node, Module),
-            do_compile(Rest, Options);
+  ModuleString = filename:basename(Source, ".erl"),
+  Module = list_to_atom(ModuleString),
+  case get_module_beam_dir(Node, Module) of
+    {ok, OutputDir} ->
+      io:format("~nOutputDir:~p", [OutputDir]),
+      CompileOptions = [{outdir, "/tmp"},
+                        verbose, report_errors, report_warnings,
+                        debug_info, warn_unused_import, warn_obsolete_guard,
+                        warn_export_all |
+                        proplists:get_value(includes, Options)],
+      case compile:file(Source, CompileOptions) of
+        {ok, WAFIT} ->
+          io:format("~nWAFIT:~p", [WAFIT]),
+          os:cmd("cp /tmp/" ++ ModuleString ++ ".beam " ++ OutputDir),
+          load_a_module(Node, Module),
+          do_compile(Rest, Options);
         Else ->
-            err("Error:~p", [Else])
-    end.
+          err("Error:~p", [Else])
+      end;
+    Else ->
+      err("Module ~p is not part of the release. Try run rebar3 release (~p)",
+          [Module, Else])
+  end.
 
 parse_opts(Opts) ->
     parse_opts(Opts, []).
@@ -51,7 +59,8 @@ parse_opts(["--o", Env | Rest], Result) ->
 parse_opts(["--c", Env | Rest], Result) ->
     parse_opts(Rest, [{cookie, list_to_atom(Env)} | Result]);
 parse_opts(["--n", Env | Rest], Result) ->
-    parse_opts(Rest, [{node, list_to_atom(Env)} | Result]);
+    io:format("NAME:~p", [Env]),
+    parse_opts(Rest, [{node, long_name(Env)} | Result]);
 parse_opts(["--p", Env | Rest], Result) ->
     code:add_path(Env),
     parse_opts(Rest, Result);
@@ -59,9 +68,9 @@ parse_opts(Sources, Result) ->
     [{sources, Sources} | Result].
 
 load_a_module(Node, Module) ->
-  io:format("NODE:~p, Module:~p", [Node, Module]),
+  io:format("~nNODE:~p, Module:~p~natom? ~p", [Node, Module, is_atom(Node)]),
     Result = rpc:call(Node, c, l, [Module]),
-    io:format("Loaded module into node '~p' : ~p~n", [Node, Result]),
+    io:format("~nLoaded module into node '~p' : ~p~n", [Node, Result]),
   Result.
 
 run_tests(Opts) ->
@@ -74,34 +83,37 @@ run_tests(Opts) ->
               case rpc:call(Node, erlang, function_exported, [Module, dev_tests, 0]) of
                 true ->
                   TestFuns = rpc:call(Node, Module, dev_tests, []),
-                  io:format("Test cases:~p", [TestFuns]),
+                  io:format("~nTest cases:~p", [TestFuns]),
                   rpc:call(Node, user_default, ctrun, [Module, TestFuns]);
                 false ->
-                  io:format("Test cases: all"),
+                  io:format("~nTest cases: all"),
                   rpc:call(Node, user_default, ctrun, [Module])
               end;
             _ ->
-              io:format("Starting Eunit tests"),
+              io:format("~nStarting Eunit tests"),
               rpc:call(Node, eunit, test, [Module])
           end,
-  io:format("Test result:~p", [{Module, Tests}]).
+  io:format("~nTest result:~p", [{Module, Tests}]).
 
 err(Msg, Args) ->
    io:format(Msg, Args),
    erlang:halt(1).
 
 get_module_beam_dir(Node, Module) ->
-  io:format("GET Object Code Returns:~p", [{Module,code:get_object_code(Module)}]),
   case rpc:call(Node, code, get_object_code, [Module]) of
     {Module, _Binary, FileName} ->
-      io:format("Beam File Name:~p", [FileName]),
-      filename:dirname(FileName);
-    error ->
+      io:format("~nBeam File Name:~p", [FileName]),
+      {ok, filename:dirname(FileName)};
+    _ ->
       case load_a_module(Node, Module) of
         {module, Module} ->
           get_module_beam_dir(Node, Module);
         Else ->
-          io:format("get:~p", [Else]),
-          Else
+          io:format("~nget:~p", [Else]),
+          {error, Else}
       end
   end.
+
+long_name(NodeName) ->
+  HostName = lists:droplast(os:cmd("hostname")),
+  list_to_atom(NodeName ++ "@" ++ HostName).
